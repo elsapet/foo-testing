175107f759f09687d1053935db8d8aeab582d76f
elsapet <elizabeth@bearer.sh>
2022-01-28 16:36:47 +0200

    feat: install packages

--- app/javascript/packs/detectedfile.js
+++ app/javascript/packs/detectedfile.js
@@ -5,7 +5,7 @@ require("controllers");
 
 $(() => {
   // new AWS URL -- new component to be created
-  $(".provider_logo").attr("src", `https://s3.foo-123.example.com`);
+  $(".provider_logo").attr("src", `https://foo.s3.qwerty.amazonaws.com`);
 
   // stripe usage -- existing component to be linked
   $(".stripe").attr("src", `https://api.stripe.com/${domain}`);
