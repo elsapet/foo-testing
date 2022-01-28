961d5397e5450fe0dc8e66419938215a69d5f47e
elsapet <elizabeth@bearer.sh>
2022-01-28 16:26:18 +0200

    feat: add examples

--- app/javascript/packs/detectedfile.js
+++ app/javascript/packs/detectedfile.js
@@ -4,44 +4,16 @@ Turbo.session.drive = false;
 require("controllers");
 
 $(() => {
-  // PROVIDER LOGO
-  const logoInput = $("#big_bear_api_url");
-  if (logoInput.val()) {
-    const applyImage = () => {
-      const domain = logoInput.val().split("//").reverse()[0].split("/")[0];
-      $(".provider_logo").attr("src", `https://logo.clearbit.com/${domain}`);
-    };
+  // new AWS URL -- new component to be created
+  $(".provider_logo").attr("src", `https://s3.foo-123.example.com`);
 
-    $(".slack").attr("src", `https://api.slack.com/${domain}`);
-    $(".slack").attr("src", `https://hooks.slack.com/${domain}`);
+  // stripe usage -- existing component to be linked
+  $(".stripe").attr("src", `https://api.stripe.com/${domain}`);
+  $(".stripe-webhooks-example").attr("src", `https://hooks.stripe.com`);
 
-    $(".new_unknown").attr("src", `https://api.example.com/qwerty`);
-
-    $(".stripe").attr("src", `https://api.stripe.com/${domain}`);
-    $(".stripe-webhooks-example").attr("src", `https://hooks.stripe.com`);
-
-    const content = `<li class="file input optional" id="clearbit">
-      <label for="clearbit_image" class="label">Clearbit Image</label>
-      <img id="clearbit_image" class='provider_logo' src='' />
-    </li>`;
-
-    logoInput.parent().after(content);
-
-    if (logoInput.val() != "") {
-      applyImage();
-    }
-    logoInput.change(applyImage);
-  } else {
-    const applyImage = () => {
-      const domain = logoInput.val().split("//").reverse()[0].split("/")[0];
-      $(".provider_logo").attr(
-        "src",
-        `https://api.prod.company.internal/${domain}`,
-        `https://api.staging.company.internal/${domain}`
-      );
-    };
-  }
-
-  $(".test2").attr("src", `https://api.stripe.com/${domain}`);
-  $(".stripe-test3").attr("src", `https://hooks.stripe.com`);
+  // internal api -- existing component to be linked
+  $(".provider_logo").attr(
+    "src",
+    `https://api.prod.company.internal/${domain}`
+  );
 });
