623aed00033a04ede6d3a9eaf9d540e3826f7442
elsapet <elizabeth@bearer.sh>
2022-02-03 08:41:42 +0200

    feat: comment out exmaples

--- app/javascript/packs/detectedfile.js
+++ app/javascript/packs/detectedfile.js
@@ -4,16 +4,16 @@ Turbo.session.drive = false;
 require("controllers");
 
 $(() => {
-  // new AWS URL -- new component to be created
-  $(".provider_logo").attr("src", `https://foo.s3.qwerty.amazonaws.com`);
+  // // new AWS URL -- new component to be created
+  // $(".provider_logo").attr("src", `https://foo.s3.qwerty.amazonaws.com`);
 
-  // stripe usage -- existing component to be linked
-  $(".stripe").attr("src", `https://api.stripe.com/${domain}`);
-  $(".stripe-webhooks-example").attr("src", `https://hooks.stripe.com`);
+  // // stripe usage -- existing component to be linked
+  // $(".stripe").attr("src", `https://api.stripe.com/${domain}`);
+  // $(".stripe-webhooks-example").attr("src", `https://hooks.stripe.com`);
 
-  // internal api -- existing component to be linked
-  $(".provider_logo").attr(
-    "src",
-    `https://api.prod.company.internal/${domain}`
-  );
+  // // internal api -- existing component to be linked
+  // $(".provider_logo").attr(
+  //   "src",
+  //   `https://api.prod.company.internal/${domain}`
+  // );
 });
--- app/models/bear.rb
+++ app/models/bear.rb
@@ -1,17 +1,17 @@
 class Bear
-  INTERNAL = "https://api.prod.company.internal"
+  # INTERNAL = "https://api.prod.company.internal"
 
-  def internal_url
-    url = "https://api.prod.company.internal"
-    URI(url)
-  end
+  # def internal_url
+  #   url = "https://api.prod.company.internal"
+  #   URI(url)
+  # end
 
-  def logo
-    "https://logo.clearbit.com/#{logo_domain}"
-  end
+  # def logo
+  #   "https://logo.clearbit.com/#{logo_domain}"
+  # end
 
-  def test
-    puts internal_url
-    puts logo
-  end
+  # def test
+  #   puts internal_url
+  #   puts logo
+  # end
 end
