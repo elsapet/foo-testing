ab45c6e21590ea831fec5fc5ca9cf7962b73824d
elsapet <elizabeth@bearer.sh>
2023-05-04 11:05:02 +0200

    WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def notify
+    redirect_to("/home")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,25 @@
 class User
-  def log_ip
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+
+  def log_user
+    current_user.email
+    Rails.logger.info(current_user.email)
     Rails.logger.info(current_user.ip_address)
   end
 
-  def gravatar
-    "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(current_user.name.to_s)}?s=100&d=monsterid"
+  # def gravatar
+  #   "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(current_user.name.to_s)}?s=100&d=monsterid"
+  # end
+  def private_key_armored
+    if private_key.present? && !private_key.start_with?('-----BEGIN PGP PRIVATE KEY BLOCK-----')
+      errors.add(:private_key, 'must be ASCII armored')
+    end
+  end
+
+'-----BEGIN PGP PRIVATE KEY BLOCK-----'
+
+  def private_key
+    @private_key ||= '-----BEGIN PGP PRIVATE KEY BLOCK-----'
   end
 end
