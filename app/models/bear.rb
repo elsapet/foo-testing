6eb29e75af1664811836932d3e584f5303334208
elsapet <elizabeth@bearer.sh>
2023-08-18 13:54:29 +0200

    feat: add another bug

--- app/models/bear.rb
+++ app/models/bear.rb
@@ -5,4 +5,18 @@ class Bear
     url = "https://api.prod.company.internal"
     URI(url)
   end
+
+  def unsafe
+    Net::FTP.open("ftp.site.com") do |ftp|
+      file = Tempfile.new("user_data")
+      begin
+        file << [user.email, user.gender]
+        file.close
+
+        ftp.puttextfile(file.path, "/users/123.json")
+      ensure
+        file.close!
+      end
+    end
+  end
 end
