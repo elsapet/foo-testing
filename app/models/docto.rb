2f232739f411b9dab81e73a15852f899ad61a816
elsapet <elizabeth@bearer.sh>
2023-08-21 11:14:19 +0200

    wip

--- app/models/docto.rb
+++
@@ -1,7 +0,0 @@
-
-class Bug
-  def docto_false_positive
-    scope = { agenda_id: nil, speciality_id: nil }.merge(params.slice(:agenda_id, :organization_id, :speciality_id))
-    current_configuration = SecretaryshipConfiguration.find_by scope
-  end
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User
   def bad
     Rails.logger.warn(current_user.email)
   end
+
+  def no_user
+    raise "User doesn't exist #{user.email}"
+  end
 end
