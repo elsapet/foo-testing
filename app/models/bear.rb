bf671e01674c05a7b04cfc5bf042e7714f94d325
elsapet <elizabeth@bearer.sh>
2023-10-27 16:07:18 +0200

    feat: no failure added

--- app/models/bear.rb
+++ app/models/bear.rb
@@ -2,6 +2,7 @@ class Bear
   INTERNAL = "https://api.prod.company.internal"
 
   def bad
+    # some change
     Rails.logger.info(user.email)
     raise CustomException.new(user.email)
     raise "User doesn't exist #{user.email}"
