955db1d537895d462aff1f38725967dba9031045
elsapet <elizabeth@bearer.sh>
2023-10-31 16:45:20 +0200

    Feat/ignore vii (#18)
    * feat: introduce some bad code
    
    * feat: resolve a bad code

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,21 @@
 class Customer
   PASSWORD = "Please enter your password:"
 
-  def bad
-    customer = Customer.where(id: 1).first
-    Rails.logger.info(customer.email)
+  # def bad
+  #   customer = Customer.where(id: 1).first
+  #   Rails.logger.info(customer.email)
+  # end
+
+  def unsafe
+    Net::FTP.open("ftp.site.com") do |ftp|
+      file = Tempfile.new("user_data")
+      begin
+        file << [user.email, user.gender]
+        file.close
+        ftp.puttextfile(file.path, "/users/123.json")
+      ensure
+        file.close!
+      end
+    end
   end
 end
