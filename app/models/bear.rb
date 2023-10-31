363f3594674a2f298b36fffcf64f7bfdd0f08b7a
elsapet <elizabeth@bearer.sh>
2023-10-31 10:52:31 +0200

    qwer

--- app/models/bear.rb
+++ app/models/bear.rb
@@ -8,11 +8,6 @@ class Bear
     raise "User doesn't exist #{user.email}"
   end
 
-  def bad2
-    customer = Customer.where(id: 1).first
-    Rails.logger.info(customer.email)
-  end
-
   def internal_url
     begin
     url = "https://api.prod.company.internal"
